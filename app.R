ce785060a16619d93c472fccab71b10135ebe89e
Sebastian Lopez-Marcano <slopezmarcano@gmail.com>
2023-07-26 14:48:27 +1000

    third and final commit for july 2023

--- app.R
+++ app.R
@@ -1,13 +1,13 @@
-library(shiny)
-#library(plotly)
-#library(lubridate)
-#library(purrr)
-#library(prophet)
-library(bslib)
-library(tidyverse)
-#library(shinydashboard)
-library(bsicons)
+# Author: Sebastian Lopez-Marcano
+# Date: 2023-07-26
 
+# Load required packages
+suppressPackageStartupMessages(library(shiny))
+suppressPackageStartupMessages(library(bslib))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(bsicons))
+
+# Define the UI cards that contain the visualizations
 cards <- list(
   card(
     full_screen = TRUE,
@@ -15,40 +15,57 @@ cards <- list(
     plotOutput("plot_ds")
   ))
 
+# Define the UI value boxes that contain the total hours per type
 value_boxes <- list(
   value_box(
     value = textOutput("total_hours_ds_box"),
     title = "Data Science Hours",
     showcase = bs_icon("activity"),
-    theme_color = "success"
+    theme_color = "success",
+    p('January to July 2023')
   ),
   value_box(
     value = textOutput("total_hours_pm_box"),
     title = "Project Management Hours",
     showcase = bs_icon("kanban"),
-    theme_color = "secondary"
+    theme_color = "success",
+    p('January to July 2023')
+  ),
+  value_box(
+    value = textOutput("total_hours_product_box"),
+    title = "Product Management Hours",
+    showcase = bs_icon("cash"),
+    theme_color = "info",
+    p('January to July 2023')
+  ),
+  value_box(
+    value = textOutput("total_hours_bi_box"),
+    title = "Business Intelligence Hours",
+    showcase = bs_icon("briefcase"),
+    theme_color = "info",
+    p('January to July 2023')
   )
   )
-
-
-
+  
+# Define the UI code
 ui <- page_sidebar(
-  title = "Time Tracking Dashboard",
+  title = "💡Time Tracking Dashboard",
+  theme = bs_theme(bootswatch = "minty"),
   sidebar = sidebar(
     div(style = "border: 1px solid #ccc; padding: 10px; margin-top: 10px;",
-        p("Welcome to the Rock Lobster Predation Behaviour Dashboard!"),
-        #p("Use the sidebar menu to navigate between different sections."),
-        p("Select a tank from the radio buttons and an event ID from the dropdown."),
+        p("Welcome to my time tracking dashboard!"),
+        p("This dashboard will show a snippet of the activities I have been doing in the last 6 months."),
+        p("The data is collected from my time tracking app and it is updated every 6 months."),
+        p("Check out TogglTrack for more information about the app")
         #p("Explore the data and visualizations for each section.")
         ),
-  ),
-  page_fillable(
-    layout_column_wrap(
-      width="10px",
-      fill = FALSE,
-      value_boxes[[1]], value_boxes[[2]]
-  )
-  ),
+    div(style = "border: 1px solid #ccc; padding: 10px; margin-top: 10px;",
+      p("Dr Sebastian Lopez-Marcano"),
+      p("Environmental Data Scientist for FishID"),
+      p("Griffith University"),
+      p("Brisbane, Australia"))),
+  layout_columns(value_boxes[[1]], value_boxes[[2]]),
+  layout_columns(value_boxes[[3]], value_boxes[[4]]),
   layout_columns(cards[[1]])
 )
 
@@ -78,18 +95,32 @@ server <- function(input, output) {
     data() %>% filter(type == "Project Management") %>% summarise(total_hours=round(sum(Duration)/60)) %>% pull() %>% as.character()
   })
 
-  # Render ggplot bar graph for Data Science and Analytics
+  # Render text with the total hours in product management type
+  output$total_hours_product_box <- renderText({
+    data() %>% filter(type == "Product Management") %>% summarise(total_hours=round(sum(Duration)/60)) %>% pull() %>% as.character()
+  })
+
+  # Render text with the total hours in business intelligence type
+  output$total_hours_bi_box <- renderText({
+    data() %>% filter(type == "Business Intelligence and Reporting") %>% summarise(total_hours=round(sum(Duration)/60)) %>% pull() %>% as.character()
+  })
+
+  # Render ggplot bar graph for the percentage of time spent on each tags for data science type
   output$plot_ds <- renderPlot({
-    data() %>%
+      data() %>%
       filter(type == "Data Science and Analytics") %>%
       group_by(tags) %>%
-      summarise(Duration = sum(Duration)/60) %>%
-      ggplot(aes(x = tags, y = Duration)) +
-      geom_bar(stat = "identity", fill = "#00BFC4") +
-      labs(title = "Data Science and Analytics", x = "Activity", y = "Duration (hours)") +
-      theme_minimal()
+      summarise(total_hours = sum(Duration) / 60) %>%
+      mutate(percentage_time = (total_hours / sum(total_hours)) * 100) %>%
+      ggplot(aes(x = reorder(tags, percentage_time), y = percentage_time)) +
+      geom_bar(stat = "identity", fill = "#ee7979c6") +
+      labs(x = "Activity", y = "Percentage Time", subtitle = "January to July 2023") +
+      theme_minimal() +
+      #theme(axis.text.x = element_text(angle = 45, hjust = 1)) %>%
+      coord_flip()
   })
 }
 
 # Return a Shiny app object
 shinyApp(ui = ui, server = server)
+
